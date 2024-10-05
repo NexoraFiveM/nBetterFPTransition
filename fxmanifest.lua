1b3dff027b1aaf300cc3a1213dc0ca2c2f6d398b
Nexora <nexora.fivem@gmail.com>
2024-10-05 12:49:13 +0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Nexora'
+description 'Better First Person Transition'
+version '1.0.0'
+
+
+client_scripts {
+    'client.lua',
+}
+
+
+shared_scripts {
+    'config.lua'
+}
+
+
+lua54 'yes'
+
